755e88d29a386bffd46841c97c35878a927504d3
Achraf Nhaili <achrafnhaili7@gmail.com>
2024-03-25 00:58:05 +0100

    pushing new challenge

--- Sort-the-odd.js
+++ Sort-the-odd.js
@@ -12,7 +12,7 @@
 
 
 
-function sortArray(array) {
+const sortArray = (array) => {
     const oddNum = array.filter((x) => x % 2).sort((a,b) => a - b)
     return array.map((x) => x % 2 ? oddNumbers.shift() : x)
 }
