30fa678fda631b5b669866e25e7272a41b1ee110
Achraf Nhaili <achrafnhaili7@gmail.com>
2024-04-10 01:14:23 +0200

    pushing new chalenge

---
+++ Sum-The-Strings.js
@@ -0,0 +1,10 @@
+// Create a function that takes 2 integers in form of a string as an input, 
+// and outputs the sum (also as a string):
+
+
+
+//soution
+
+
+
+const sumStr = (a,b) => String(Number(a) + Number(b)) 
