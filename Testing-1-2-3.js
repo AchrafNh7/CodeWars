d21331ab6408e1defeb7169b96277a7a5aa78e66
Achraf Nhaili <achrafnhaili7@gmail.com>
2024-05-02 13:37:51 +0200

    pushing new challenge

---
+++ Testing-1-2-3.js
@@ -0,0 +1,9 @@
+// The numbering starts at 1. The format is n: string. Notice the colon and 
+// space in between.
+
+
+
+//solution
+
+
+const number = array => array.map((el,i)=> `${i + 1}: ${el}`)
