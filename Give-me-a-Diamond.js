b281c58098ac8a1f17907f9f82e5a6c0eb8b00ba
Achraf Nhaili <achrafnhaili7@gmail.com>
2024-05-06 13:52:11 +0200

    pushing new chalenge

---
+++ Give-me-a-Diamond.js
@@ -0,0 +1,29 @@
+// Jamie is a programmer, and James' girlfriend. She likes diamonds, and wants a 
+// diamond string from James. Since James doesn't know how to make this happen, 
+// he needs your help.
+// Task
+// You need to return a string that looks like a diamond shape when printed on 
+// the screen, using asterisk (*) characters. Trailing spaces should be removed, 
+// and every line must be terminated with a newline character (\n).
+// Return null/nil/None/... if the input is an even number or negative, as it is 
+// not possible to print a diamond of even or negative size.
+
+
+
+//solution
+
+
+function diamond(n){
+    if ((n >= 3) && (n % 2 !== 0)){
+     let firstHalf = []
+    for (let i = 0; i < (n-1)/2; i++) {
+      firstHalf.push(" ".repeat(((n-1)/2) -i) + "*".repeat(i * 2 + 1) + "\n");
+    }
+    return [...firstHalf, "*".repeat(n) + "\n" ,...firstHalf.reverse()].join("");
+    }else if(n == 1){
+      return '*\n'
+    }else{
+      return null
+    }
+  
+  }
