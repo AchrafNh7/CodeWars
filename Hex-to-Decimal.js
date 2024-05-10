a538cb5dc4a7048c769ac520f6d99fbbe0b0cee1
Achraf Nhaili <achrafnhaili7@gmail.com>
2024-05-10 17:07:36 +0200

    pushing new challenge

---
+++ Hex-to-Decimal.js
@@ -0,0 +1,11 @@
+// Complete the function which converts hex number (given as a string) to a 
+// decimal number.
+
+
+
+//solution
+
+
+function hexToDec(hexString){
+    return parseInt(hexString, 16)
+  }
