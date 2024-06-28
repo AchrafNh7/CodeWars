976621699e66c04a40f18292bd45eb2d3f8d3799
Achraf Nhaili <achrafnhaili7@gmail.com>
2024-06-28 13:02:46 +0200

    pushing new challenge

---
+++ Grasshopper-Combine-strings.js
@@ -0,0 +1,10 @@
+// Create a function named combineNames that accepts two parameters 
+// (first and last name). The function should return the full name.
+
+
+
+//solution
+
+const combineNames = (firstName, lastName) =>{
+    return `${firstName} ${lastName}`
+  }
