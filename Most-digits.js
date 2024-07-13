1def3239022e6a69d37c4aacca04764884cc98ac
Achraf Nhaili <achrafnhaili7@gmail.com>
2024-07-13 14:15:25 +0200

    pushing new challenge

---
+++ Most-digits.js
@@ -0,0 +1,11 @@
+// Find the number with the most digits.
+// If two numbers in the argument array have the same number of digits, 
+// return the first one in the array.
+
+
+
+//solution
+
+function findLongest(array){
+    return array.sort((a,b)=> String(b).length - String(a).length)[0]
+}
